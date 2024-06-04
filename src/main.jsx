97065e97c6893b754a75d3b248be40f8757e89b6
Charlotte Varlet <c.varlet@3wa.fr>
2024-06-04 11:10:14 +0200

    test

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router basename={'https://charlottevarlet3wa.github.io'}>
       <App />
-    </Router>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hashrouter_test/' // Replace <repository-name> with your GitHub repository name
+  base: '/hashrouter_test/' 
 })
