0d090cb59a6149983aaf3f9b434db0820fc5fa36
Charlotte Varlet <c.varlet@3wa.fr>
2024-06-04 11:00:48 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-
+    <>
+    <NavBar />
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </HashRouter>
+    </>
   );
 }
 
